3d11aedc1b787e9eae5f320639aadc893efebf07
kaue <ksoaresmadarazzo@gmail.com>
2025-08-29 09:52:21 -0300

    criando nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Error from './routes/Error'
 import Home from './routes/Home'
 import Cliente from './routes/Cliente'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 function App() {
   
 
   return (
     //HABILITA A NAVEGAÇÃO POR ROTAS NA APLICAÇÃO
     <BrowserRouter>
+    <Nav/>
     <main>
       {/* Gerencia a exibição dos componentes com base na url */}
       <Routes>
@@ -21,8 +24,9 @@ function App() {
 
       </Routes>
     </main>
-     
+    <Footer/> 
     </BrowserRouter>
+    
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import  {Link} from 'react-router-dom'
+import logo from '../assets/imagem.png'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header>
+      <nav>
+        <img src={} alt="logo" />
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+            
+          </li>
+          <li>
+            <Link to='/'>Cliente</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
