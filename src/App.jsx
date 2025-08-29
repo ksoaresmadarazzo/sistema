2aa02d4ff83193c8a4f88c81c9989deda6cd0f0d
kaue <ksoaresmadarazzo@gmail.com>
2025-08-29 09:28:17 -0300

    criando as rotas do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Error from './routes/Error'
+import Home from './routes/Home'
+import Cliente from './routes/Cliente'
 function App() {
   
 
   return (
-    <>
+    //HABILITA A NAVEGAÇÃO POR ROTAS NA APLICAÇÃO
+    <BrowserRouter>
+    <main>
+      {/* Gerencia a exibição dos componentes com base na url */}
+      <Routes>
+        {/* ROTA PARA LIDAR COM AS URL NÃO ENCONTRADAS */}
+        <Route path='*' element={<Error/>}/>
+
+        <Route path='/' element={<Home/>}/>
+
+          <Route path='/cliente' element={<Cliente/>}/>
+        
+
+      </Routes>
+    </main>
      
-    </>
+    </BrowserRouter>
   )
 }
 
